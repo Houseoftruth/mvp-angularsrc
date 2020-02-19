ddfbebbfdcb11bf6d957f654bc8b3611b4677c41
ohrha <trigramthree@gmail.com>
2020-02-18 19:00:39 -0800

    'INITIAL-COMMIT'

---
+++ .aws-sam/build/addToKitchenLog/app.js
@@ -0,0 +1,57 @@
+// const axios = require('axios')
+// const url = 'http://checkip.amazonaws.com/';
+const AWS = require('aws-sdk')
+
+AWS.config.update({ region: "eu-central-1" });
+
+
+let response;
+
+/**
+ *
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * @param {Object} context
+ *
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ *    
+ */
+exports.lambdaHandler = async (event, context) => {  
+
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region: "eu-central-1" });
+ 
+    const params = { 
+        TableName: "mvp-chatlog",
+        Item: {
+            date: Date.now(),
+            message: "Coming right up! :)!"
+        }
+    }
+
+    try {
+
+        const data = await documentClient.put(params).promise();
+        
+
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'Log entry successfully saved...',
+            })
+        }
+        return response
+
+
+    } catch (err) {
+
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'There was an error adding new log entry...',
+            'error': JSON.stringify(err)
+            })
+        }
+        return response
+
+    }
+};
