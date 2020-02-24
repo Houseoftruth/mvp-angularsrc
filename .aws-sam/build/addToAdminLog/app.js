7134c6787caf05f6d2273a74bb6d80e3c6e443ef
ohrha <trigramthree@gmail.com>
2020-02-24 14:18:46 -0800

    'ADD-FILES-TO-REPO'

---
+++ .aws-sam/build/addToAdminLog/app.js
@@ -0,0 +1,56 @@
+const AWS = require('aws-sdk')
+
+AWS.config.update({ region: "eu-central-1" });
+
+
+let response;
+
+/**
+ *
+ * @param {Object} event - API Gateway Lambda Proxy Input Format
+ *
+ * @param {Object} context
+ *
+ * @returns {Object} object - API Gateway Lambda Proxy Output Format
+ * 
+ */
+exports.lambdaHandler = async (event, context) => {
+
+    const documentClient = new AWS.DynamoDB.DocumentClient({ region: "eu-central-1" });
+
+    const params = {
+
+        TableName: "mvpchatlog",
+        Item: {
+            date: Date.now(),
+            message: "Richard isn't going to be in today, just to let you know..."
+        }
+    }
+
+    try {
+
+        const data = await documentClient.put(params).promise();
+        
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'Log entry successfully saved...',
+            })
+        }
+
+        return response
+
+    } catch (err) {
+
+        response = {
+            'statusCode': 200,
+            'body': JSON.stringify({
+                message: 'There was an error adding new log entry...',
+            'error': JSON.stringify(err)
+            })
+        }
+
+        return response
+        
+    }
+};
